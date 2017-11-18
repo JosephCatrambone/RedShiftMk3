3057ffe7ec2b54531bc69e6f6a0297af60fc9bd5
Joseph Catrambone <jo.jcat@gmail.com>
2017-11-18 13:19:33 -0800

    First import.

---
+++ desktop/src/io/xoana/redshift/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package io.xoana.redshift.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import io.xoana.redshift.GDXMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GDXMain(), config);
+	}
+}
